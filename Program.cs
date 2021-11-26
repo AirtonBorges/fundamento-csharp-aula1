e168d1af8b2097859bb9c0dea5335b61ee9124ff
Arto <airtonborges2211@gmail.com>
2021-11-26 20:59:17 +0000

    Exercicio 2 - Senso 2021

--- Program.cs
+++ Program.cs
@@ -1,60 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
+
+
 
 namespace dotnetcore
 {
     class Program
     {
-        const float NOTA_PRA_PASSAR = 7;
+        const float MAIORIDADE = 18;
 
         static void Main(string[] args)
         {
-            var notasBronza = new List<float>();
-            float mediaBronza = 0;
-
-            var notasZuqui = new List<float>();
-            float mediaZuqui = 0;
-
-            Console.WriteLine("-- Bem vindo ao final do ano --");
+            var pessoas = new List<Pessoa>();
+            pessoas.Add(new Pessoa("Adaleberto", 10));
+            pessoas.Add(new Pessoa("Cleitin Gameplays", 16));
+            pessoas.Add(new Pessoa("Sasuke Naruto", 19));
             
-            Console.WriteLine("\nZuqui, Digite suas notas: ");
-            notasZuqui = ReceberNotas(3);
-            mediaZuqui = CalcularMedia(notasZuqui);
-            
-            Console.WriteLine("\nBronza, Digite suas notas: ");
-            notasBronza = ReceberNotas(2);
-            mediaBronza = CalcularMedia(notasBronza);
-
-            Console.WriteLine("--- Resultados ----");
-            Console.WriteLine(MensagemFinal("Zuqui", mediaZuqui));
-            Console.WriteLine(MensagemFinal("Bronza", mediaBronza));
-        }
-
-        private static string MensagemFinal(string nome, float media) {
-            var resultado = media >= NOTA_PRA_PASSAR ? "passou!" : "reprovou, estude mais no proximo ano.";
-            return $"{nome}, você tirou {media} e {resultado}";
-        }
-
-        private static List<float> ReceberNotas(int pTam) {
-            var notas = new List<float>();
-            
-            for (int i = 0; i < pTam; i++) {
-                Console.Write($"> Nota {i + 1}: ");
-                notas.Add(float.Parse(Console.ReadLine()));
+            Console.WriteLine("---- SENSO 2021 ----");
+            Console.WriteLine("- Participantes: ");
+            foreach (var pessoa in  pessoas) {
+                Console.WriteLine($"> {pessoa.Nome}, Idade {pessoa.Idade}.");
             }
 
-            return notas;
+            int quantidadePessoasMaiorIdade = QuantidadePessoasMaioresDeIdade(pessoas);
+            Pessoa pessoaMaisVelha = PessoaMaisVelha(pessoas);
+            Pessoa pessoaMaisNova = PessoaMaisNova(pessoas);
+            Console.WriteLine("\n----- DADOS -----");
+            Console.WriteLine($"- Media de idade do grupo: {CalcularMediaIdades(pessoas)}");   
+            Console.WriteLine($"- Pessoa mais velha: {pessoaMaisVelha.Nome}, com {pessoaMaisVelha.Nome}");
+            Console.WriteLine($"- Pessoa mais velha: {pessoaMaisNova.Nome}, com {pessoaMaisNova.Idade}");
+            Console.WriteLine($"- Quantidade de Pessoas Maiores de Idade: {quantidadePessoasMaiorIdade}\n");
         }
 
-        private static float CalcularMedia(List<float> notas) {
-            var total = 0;
-            
-            foreach(var nota in notas)
-            {
-                total += (int) nota;
-            }
-            
-            return total / notas.Count;
+        private static int CalcularMediaIdades(List<Pessoa> pPessoas) {
+            return (int)pPessoas.Average(p => p.Idade);
+        }
+
+        private static Pessoa PessoaMaisVelha(List<Pessoa> pPessoas) {
+            return pPessoas.OrderBy(p => p.Idade).First();
+        }
+
+        private static Pessoa PessoaMaisNova(List<Pessoa> pPessoas) {
+            return pPessoas.OrderBy(p => p.Idade).Last();
+        }
+
+        private static int QuantidadePessoasMaioresDeIdade(List<Pessoa> pPessoas) {
+            return pPessoas.Where(p => p.Idade > MAIORIDADE).Count(); 
+        }
+    }
+
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }      
+
+        public Pessoa(string pNome, int pIdade)
+        {
+            Nome = pNome;
+            Idade = pIdade;                
         }
     }
 }
