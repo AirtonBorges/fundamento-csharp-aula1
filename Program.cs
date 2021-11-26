ad0fc020101ae9305bcfce5d3588b592aa640918
Arto <airtonborges2211@gmail.com>
2021-11-26 19:53:44 +0000

    Exercicio 1 - Testar se Passou

--- Program.cs
+++ Program.cs
@@ -1,36 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dotnetcore
 {
     class Program
     {
-        // Exercício 1:
-        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
-        // - Imprimir no console para cada aluno
-        //      - Nome do aluno
-        //      - Caso média maior ou igual 7:
-        //          - Passou no ano letivo
-        //          - Não passou no ano letivo
-        // Requisítos:
-        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
-        // - Utilizar For ou Foreach
+        const float NOTA_PRA_PASSAR = 7;
 
-
-        // Exercício 2:
-        // - Fazer um programa que cadastre pessoas, contendo nome e idade;
-        // - Imprimir a media de idade das pessoas
-        // - Imprimir o nome e idade da pessoa mais velha
-        // - Imprimir o nome e idade da pessoa mais nova
-        // - Imprimir a quantidade de pessoas com maior idade
-        // Requisítos:
-        // - Utilizar List (Avg, Max, Min, Where)
-        // - Utilizar Classe Pessoa
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando Programa");
+            var notasBronza = new List<float>();
+            float mediaBronza = 0;
 
-            var xNotasZuqui = new int[] { 7, 6, 5, 3 };
-            var xNotasBronza = new int[] { 8, 9, 10, 8 };
+            var notasZuqui = new List<float>();
+            float mediaZuqui = 0;
+
+            Console.WriteLine("-- Bem vindo ao final do ano --");
+            
+            Console.WriteLine("\nZuqui, Digite suas notas: ");
+            notasZuqui = ReceberNotas(3);
+            mediaZuqui = CalcularMedia(notasZuqui);
+            
+            Console.WriteLine("\nBronza, Digite suas notas: ");
+            notasBronza = ReceberNotas(2);
+            mediaBronza = CalcularMedia(notasBronza);
+
+            Console.WriteLine("--- Resultados ----");
+            Console.WriteLine(MensagemFinal("Zuqui", mediaZuqui));
+            Console.WriteLine(MensagemFinal("Bronza", mediaBronza));
+        }
+
+        private static string MensagemFinal(string nome, float media) {
+            var resultado = media >= NOTA_PRA_PASSAR ? "passou!" : "reprovou, estude mais no proximo ano.";
+            return $"{nome}, você tirou {media} e {resultado}";
+        }
+
+        private static List<float> ReceberNotas(int pTam) {
+            var notas = new List<float>();
+            
+            for (int i = 0; i < pTam; i++) {
+                Console.Write($"> Nota {i + 1}: ");
+                notas.Add(float.Parse(Console.ReadLine()));
+            }
+
+            return notas;
+        }
+
+        private static float CalcularMedia(List<float> notas) {
+            var total = 0;
+            
+            foreach(var nota in notas)
+            {
+                total += (int) nota;
+            }
+            
+            return total / notas.Count;
         }
     }
 }
